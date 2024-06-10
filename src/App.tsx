73ef031bd3e6cb64ab8ae8963d53716ee6231fd4
Amit Dubey <iamsaagar@yahoo.com>
2024-06-10 23:44:31 +0100

    Update

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "./components/page.layout";
+
+function App() {
+  return (
+    <PageLayout>
+      <h1>Demo</h1>
+    </PageLayout>
+  );
+}
+
+export default App;
---
+++ src/components/page.layout.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import PageSideMenu from "./page.sideMenu";
+import PageMainMenu from "./page.mainMenu";
+
+interface PageLayoutProps {
+  children: JSX.Element;
+}
+
+const PageLayout: FC<PageLayoutProps> = ({ children }) => {
+  return (
+    <div className="layout-wrapper layout-content-navbar  ">
+      <div className="layout-container">
+        <PageSideMenu />
+        <div className="layout-page">
+          <PageMainMenu />
+          {children}
+        </div>
+      </div>
+
+      <div className="layout-overlay layout-menu-toggle"></div>
+
+      <div className="drag-target"></div>
+    </div>
+  );
+};
+
+export default PageLayout;
